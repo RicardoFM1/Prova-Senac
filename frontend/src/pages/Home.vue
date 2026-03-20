5268f9776484aaec176cbd320f0071077a4d2564
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2026-03-20 17:13:37 -0300

    adicionado frontend

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+
+
+<v-sheet>
+Gerenciamento de produtos
+</v-sheet>
+
+<v-data-table
+:headers=headers>
+</v-data-table>
+
+</template>
+
+<script setup>
+const headers = [
+    {title: "Nome", value: "nome"}
+]
+</script>
