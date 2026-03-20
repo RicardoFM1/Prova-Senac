73506a3fc862f491b3c61c8b96f4f6b6e53eb5c1
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2026-03-20 17:21:09 -0300

    documentação adicionada

--- Backend/Controllers/ProdutoController.php
+++ Backend/Controllers/ProdutoController.php
@@ -6,27 +6,27 @@ class ProdutoController {
     public $service;
 
     public function _construct() {
-        $this->db = db();
+        $this->$service = new ProdutoService();
     }
 
     public function listarProdutos () {
-        echo json_encode($this->listarProdutos());
+        echo json_encode($this->service->listarProdutos());
     }
 
     public function criarProduto () {
         $dados = json_decode(file_get_contents("php://input"), true);
-        echo json_encode($this->criarProduto($dados));
+        echo json_encode($this->service->criarProduto($dados));
     }
 
     public function atualizarProduto () {
         $dados = json_decode(file_get_contents("php://input"), true);
         $produtoId = $_GET['produto_id'];
-        echo json_encode($this->criarProduto($dados, $produtoId));
+        echo json_encode($this->service->criarProduto($dados, $produtoId));
     }
 
     public function deletarProduto () {
         $produtoId = $_GET['produto_id'];
-        echo json_encode($this->criarProduto($produtoId));
+        echo json_encode($this->service->criarProduto($produtoId));
     }
   
     
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Prova-Senac
+
+
+O intuito é de fazer um sistema que possa ser realizado o CRUD de produtos com administração de usuarios/permissões
+
+# Como rodar:
+
+Backend: 
+Entre na pasta do backend -> cd backend
+E depois na pasta de Rotas -> cd Routes
+E então rode o php local -> php -S localhost:8000
+
+Frontend: 
+Entre na pasta do frontend -> cd frontend
+Instale se necessário as dependências -> npm i
+E então inicie o vuetify -> npm run dev
+
+
+
+# Ferramentas/Linguagens/Frameworks utilizados:
+
+Backend: 
+PHP puro
+
+Frontend:
+Vuetify (com vue) 
+
+
